0adef787b395b5cfec4b0730e4a72ba2da165192
Hell-Tractor <1104840448@qq.com>
2025-04-15 15:17:42 +0800

    modified money format

--- src/common/Money.ts
+++ src/common/Money.ts
@@ -9,7 +9,7 @@ export class Money {
 
     public toString() : string {
         // format the money amount to 2 decimal places with comma separated thousands
-        return `${(this._amount / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} ${this._currency.symbol}`;
+        return `${this._currency.symbol}${(this._amount / 100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`;
     }
 }
 
